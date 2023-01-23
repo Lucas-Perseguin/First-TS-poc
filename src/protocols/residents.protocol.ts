d2a6d571ebd4489ac91156537ba11dda1e5851ba
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2023-01-23 16:33:28 -0300

    fix: set parameters as optional

--- src/protocols/residents.protocol.ts
+++ src/protocols/residents.protocol.ts
@@ -9,7 +9,7 @@ export type ResidentEntity = {
 export type Resident = Omit<ResidentEntity, 'id' | 'isActive'>;
 
 export type ResidentsQueries = {
-  name: string | string[] | QueryString.ParsedQs | QueryString.ParsedQs[];
-  id: string | string[] | QueryString.ParsedQs | QueryString.ParsedQs[];
-  isActive: string | string[] | QueryString.ParsedQs | QueryString.ParsedQs[];
+  name?: string | string[] | QueryString.ParsedQs | QueryString.ParsedQs[];
+  id?: string | string[] | QueryString.ParsedQs | QueryString.ParsedQs[];
+  isActive?: string | string[] | QueryString.ParsedQs | QueryString.ParsedQs[];
 };
